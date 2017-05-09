1bd170c31845eeaf56ce5fd7721f89db776ca78c
Rasmus Selsmark <rasmus@unity3d.com>
2017-05-09 15:16:29 +0300

    Enabled Ads and added Ads game object showing an ad

---
+++ Assets/Ads.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ads : MonoBehaviour
+{
+    void Start () 
+    {
+        #if UNITY_ADS
+        StartCoroutine (ShowAdWhenReady());
+        #endif
+    }
+
+    #if UNITY_ADS
+    IEnumerator ShowAdWhenReady()
+    {
+        while (!UnityEngine.Advertisements.Advertisement.IsReady())
+            yield return null;
+
+        UnityEngine.Advertisements.Advertisement.Show ();
+    }
+    #endif
+}
